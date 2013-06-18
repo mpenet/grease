a46fe8acc85267cb2d78d80812e2e3da2057456a
Max Penet <m@qbits.cc>
2013-06-18 09:45:29 +0200

    Force compile target to 1.6 in case javac 1.7x is used

--- project.clj
+++ project.clj
@@ -4,15 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles {:1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
+    :profiles {:1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5  {:dependencies [[org.clojure/clojure "1.5.0"]]}
              :1.6  {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
              :dev  {:dependencies []}
              :test  {:dependencies []}}
-  ;; :codox {:src-dir-uri "https://github.com/mpenet/grease/blob/master"
-  ;;         :src-linenum-anchor-prefix "L"
-  ;;         :output-dir "doc/codox"}
   :source-paths      ["src/clj"]
   :java-source-paths ["src/java"]
-  ;; :warn-on-reflection true
-  )
+  :javac-options     ["-target" "1.6" "-source" "1.6"])
